786ec148d3ac504a15eee078e60348852139c25a
rakshith22-glitch <rakshith22@outlook.com>
2024-06-10 11:46:30 -0700

    git hub

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/rakshith22-glitch/portfolio"
+                href="https://github.com/rakshith22-glitch/"
                 target="_blank"
                 className="fork-btn-inner"
               >
