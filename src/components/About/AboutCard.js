e79263a6eb1483532d2e66dc98c4354a66e774ce
rakshith22-glitch <rakshith22@outlook.com>
2024-06-10 11:37:44 -0700

    latest push

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,22 +8,24 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            HEY  !!! I am{" "}
-            <span className="purple">RAKSHITH</span>
-            <br />I am passionate about coding and building applications and websites to help others start their dream businesses.
-            <span className="purple"> Based in Santa Monica, California</span>
+            <br />
+             I specialize in building professional websites tailored to your business needs.
             <br />
             <br />
-            Besides coding, I love to:
+            <span className="purple">
+              Based in Santa Monica, California
+            </span>
+            <br />
+            Here's a little bit about me:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Play Pickleball, Hike, Read, Workout, Chill at coffee shops and Travel
+              <ImPointRight /> I enjoy playing Pickleball, hiking, reading, working out, relaxing at coffee shops, and traveling.
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "If you're going to try go all the way, otherwise dont even start !"
+            "If you're going to try, go all the way; otherwise, don't even start."
           </p>
           <footer className="blockquote-footer">Rakshith</footer>
         </blockquote>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -16,7 +16,7 @@ function Projects() {
     <Container fluid className="project-section">
       <Particle />
       <Container>
-        <h2 className="project-heading">Shopify, Wix, and Square Space Apps</h2>
+        <h2 className="project-heading">Shopify, Wix, and Square Space Websites</h2>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
